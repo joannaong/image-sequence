aa509c029dee39930239d5bc10702ddcd03b0ac6
joanna ong <ongjfu@gmail.com>
2014-04-30 18:59:26 -0400

    modifications

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,68 +3,60 @@
 /*
  *************************************************************
  * INSTRUCTIONS
- *
- * Go to your terminal and use "grunt" to deploy the site
+ * 
+ * Go to your terminal and use "grunt" to deploy the site 
  * locally and start developing
  *
  *************************************************************
  */
 
 module.exports = function(grunt) {
-    /*
-     * Used for loading all of your grunt plugins
-     */
+    // Used for loading all of your grunt plugins
     require('load-grunt-tasks')(grunt);
 
-    /*
-     *************************************************************
-     * Grunt config
-     *************************************************************
-     */
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        /*
-         * An array list of all your js files to be minified in compiling order
-         */
-        scripts: {
-            cdn: [],
-            libs: [
-                'bower_components/jquery/dist/jquery.min.js'
-            ],
-            app: [
-                'src/js/app/AppMain.js'
-            ]
-        },
-
-        /*
-         * Config variables for different deployment environments
-         */
-        config: {
-            local: {
-                options: {
-                    variables: {
-                        environment: {
-                            id: "local",
-                            host: "localhost",
-                            dest: "dist/local/",
-                            dir: "/"
-                        }
+    // Config variables for different deployment environments
+    var config = {
+        local: {
+            options: {
+                variables: {
+                    environment:{
+                        id: "local",
+                        host: "localhost",
+                        dest: "dist/local/",
+                        dir: "/"
                     }
                 }
             }
         },
+        dev: {},
+        test: {},
+        stage: {},
+        prod: {}
+    }
 
-        /*
-         *************************************************************
-         * Grunt processes
-         *************************************************************
-         */
+    // An array list of all your js files to be minified in compiling order
+    var scripts = {
+        lib_head:[
+            'bower_components/modernizr/modernizr.js'
+        ],
+        lib_foot:[
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/jquery/dist/jquery-mousewheel.js'
+        ],
+        app:[
+            'src/js/**/*.js'
+        ]
+    }
 
-        /*
-         * Delete all files in the distribution directory
-         *
-         */
+    //----------------
+    //- Grunt config
+    //----------------
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        config: config,
+        scripts: scripts,
+
+        // Delete all files in the distribution directory
         clean: {
             dist: {
                 src: [
@@ -73,96 +65,70 @@ module.exports = function(grunt) {
             }
         },
 
-        /*
-         * Copy the asset directory
-         * OR Copy the js directory if you are developing to preserve all your file paths
-         */
-        copy: {
-            assets: {
-                files: [{
-                    expand: true,
-                    cwd: "src/asset/",
-                    src: ["**"],
-                    dest: "<%= environment.dest %>asset/"
-                }]
-            },
-            js: {
-                files: [{
-                    expand: true,
-                    cwd: "src/js/",
-                    src: ["**"],
-                    dest: "<%= environment.dest %>js/"
-                }]
-            }
-        },
+        // Copy the asset directory
+        // copy: {
+        //     assets: {
+        //         files: [{
+        //             expand: true,
+        //             cwd: "src/asset/",
+        //             src: ["**"],
+        //             dest: "<%= environment.dest %>asset/"
+        //         }]
+        //     }
+        // },
 
-        /*
-         * SASS compilation
-         */
+        // SASS compilation
         sass: {
-            debug: {
+            dist: {
                 options: {
-                    style: 'expanded'
+                    sourcemap: true
                 },
-                files: {
+                files:{
                     "<%= environment.dest %>css/main.css": "src/css/main.sass"
                 }
-            },
-            compile: {
-                files: {
-                    "<%= environment.dest %>css/main-min.css": "src/css/main.sass"
-                }
             }
         },
 
-        /*
-         * JS compilation
-         */
+        // compass: {
+        //     dist: {
+        //         options: {
+        //             sassDir: 'src/css',
+        //             cssDir: '<%= environment.dest %>css',
+        //             noLineComments: true,
+        //             sourcemap: true
+        //         }
+        //     }
+        // },
+
+        // JS compilation 
         uglify: {
             dist: {
-                options: {
-                    drop_console: true,
-                    wrap: '<%= pkg.name %>'
-                },
                 files: {
-                    '<%= environment.dest %>js/main-min.js': '<%= scripts.app %>'
+                    '<%= environment.dest %>js/main-min.js': '<%= scripts.app %>',
+                    '<%= environment.dest %>js/libfoot-min.js': '<%= scripts.lib_foot %>',
+                    '<%= environment.dest %>js/libhead-min.js': '<%= scripts.lib_head %>'
                 }
             }
         },
 
-        /*
-         * Concat all js libs into one lib file
-         */
-        concat: {
-            dist: {
-                src: '<%= scripts.libs %>',
-                dest: '<%= environment.dest %>js/libs.js'
-            }
-        },
-
-        /*
-         * Jade compilation
-         * - Modify the scripts array to remove the src directory path
-         */
+        // Jade compilation
+        // - Modify the scripts array to remove the src directory path
         jade: {
             dist: {
                 options: {
                     pretty: false,
                     data: {
                         environment: "<%= environment %>",
-                        scripts: "<%= scripts %>",
                         srcDir: "src/"
                     }
                 },
                 files: {
-                    "<%= environment.dest %>index.html": "src/template/page/home.jade"
+                    "<%= environment.dest %>index.html": "src/template/pages/*"
                 }
             }
         },
 
-        /*
-         * Live reload your browser when developing
-         */
+        // Live reload your browser when developing
         connect: {
             options: {
                 port: 9000,
@@ -179,34 +145,25 @@ module.exports = function(grunt) {
             }
         },
 
-        /*
-         * Watch files and update only changed content
-         * - Added grunt-newer to compile only the jade file that pertains to what was changed
-         */
+        // Watch files and update only changed content
+        // - Added grunt-newer to compile only the jade file that pertains to what was changed
         watch: {
             jade: {
                 files: ['src/template/**/*.jade'],
-                tasks: ['config:local', 'jade']
+                tasks: ['config:local', 'newer:jade']
             },
-            sass: {
+            compass: {
                 files: ['src/css/**/*.sass'],
-                tasks: ['config:local', 'sass:debug']
+                tasks: ['config:local', 'sass']
             },
             js: {
                 files: ['src/js/**/*.js'],
-                tasks: ['config:local', 'copy:js']
+                tasks: ['config:local', 'newer:uglify']
             },
-            gruntfile: {
-                files: ['Gruntfile.js'],
-                tasks: [
-                    'config:local',
-                    'clean',
-                    'copy:assets',
-                    'copy:js',
-                    'sass:debug',
-                    'jade'
-                ]
-            },
+            // copy: {
+            //     files: ['src/asset/**/*.{json,png,jpg,jpeg,gif,webp,svg,eot,ttf,woff,mp3,wav,swf,mp4,webm,ogv}'],
+            //     tasks: ['config:local', 'newer:copy']
+            // },
             livereload: {
                 options: {
                     livereload: '<%= connect.options.livereload %>'
@@ -217,38 +174,29 @@ module.exports = function(grunt) {
     });
 
 
-    /*
-     *************************************************************
-     * Grunt tasks to be run through your terminal
-     *************************************************************
-     */
+    //----------------------------------------------
+    //- Grunt tasks to be run through your terminal
+    //----------------------------------------------
 
-    /*
-     * The default "grunt" task is for active development
-     */
+    // The default "grunt" task is for active development
     grunt.registerTask('default', [
         'config:local',
         'clean',
-        'copy:assets',
-        'copy:js',
-        'concat',
-        'sass:debug',
+        'uglify',
+        'sass',
         'jade',
         'connect:livereload',
         'watch'
     ]);
 
-    /*
-     * Compile build for the different deployment environments
-     */
+    // Compile build for the different deployment environments
     grunt.registerTask('build', function(_environment) {
         grunt.task.run([
-            'config:' + _environment,
+            'config:'+_environment,
             'clean',
             'copy:assets',
             'uglify',
-            'concat',
-            'sass:compile',
+            'sass',
             'jade'
         ]);
     });
